e2d5ecdee21804c884aaae4c5118a23e24ea3509
oscardasilvarodriguez <grelodj@hotmail.com>
2025-02-13 09:39:47 +0100

    ejercicio32-java corregido

--- src/main/java/com/campusdual/classroom/Exercise32.java
+++ src/main/java/com/campusdual/classroom/Exercise32.java
@@ -1,7 +1,6 @@
 package com.campusdual.classroom;
 
 import com.campusdual.util.Utils;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,13 +9,20 @@ public class Exercise32 {
 
     public static void main(String[] args) {
 
+        printToFile(generateStringToSave(generateUserInputToSave()));
     }
 
     public static String generateStringToSave(String string) {
 
+        if (string == null) {
+            return generateUserInputToSave();
+        } else {
+            return string;
+        }
     }
 
     private static String generateUserInputToSave(){
+
         StringBuilder sb = new StringBuilder();
         System.out.println("Escribe debajo el texto que quieras. Pulsa \"ENTER\" 2 veces seguidas para finalizar:");
         String string;
@@ -28,6 +34,11 @@ public class Exercise32 {
 
     public static void printToFile(String string) {
 
+        try(BufferedWriter bw = new BufferedWriter(new PrintWriter("src/main/resources/data.txt"))) {
+            bw.write(string);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
